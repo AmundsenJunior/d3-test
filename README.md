864f97cd7de23370175f610d57267d3b601b6aea
Scott Russell <scottedwardrussell@gmail.com>
2014-10-27 17:39:07 -0400

    Added build and server scripts, test site page; altered directory structure

---
+++ README.md
@@ -0,0 +1,10 @@
+## build/
+Execute the scripts from the project root directory in the following order:
+1. na-download.sh
+2. na-unzip.sh
+3. na-convert-json.sh
+
+## .gitignore
+Keeps ```data/``` and ```node_modules/``` from version control.
+Follow [TUTORIAL.md](TUTORIAL.md) for project deployment instructions.
+
---
+++ build/na-convert-json.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+if [ ! -d "data/geo/geojson" ]; then
+  mkdir data/geo/geojson
+  echo "geojson directory created"
+fi
+
+if [ ! -d "data/geo/topojson" ]; then
+  mkdir data/geo/topojson
+  echo "topojson directory created."
+fi
+
+ogr2ogr \
+  -f GeoJSON \
+  -where "ADM0_A3 IN ('USA', 'CAN')" \
+  data/geo/geojson/subunits.json \
+  data/geo/shapefile/ne_10m_admin_0_map_subunits/ne_10m_admin_0_map_subunits.shp
+
+ogr2ogr \
+  -f GeoJSON \
+  -where "ISO_A2 IN ('US', 'CA') AND SCALERANK < 8" \
+  data/geo/geojson/places.json \
+  data/geo/shapefile/ne_10m_populated_places/ne_10m_populated_places.shp
+
+topojson \
+  -o data/geo/topojson/na.json \
+  --id_property SU_A3 \
+  --properties name=NAME \
+  -- \
+  data/geo/geojson/subunits.json \
+  data/geo/geojson/places.json
+
+
